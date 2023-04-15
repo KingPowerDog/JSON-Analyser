effe61da26d73e0ce3080f78a534c6e8faa4d270
KingPowerDog <115365205+KingPowerDog@users.noreply.github.com>
2023-04-16 01:48:07 +0800

    Initial Upload of files
    First version of files + updated readme

---
+++ JSON_Analyser.py
@@ -0,0 +1,92 @@
+# This python script reads a source json file and applies rules to be able to generate a basic analysis to CSV format
+# Requres 2 other files:
+#    rules.json: contains the rules for analysis
+#    output.cfg: defines the CSV format, such as separator and marker value
+
+import json
+import csv
+import os
+import array as arr
+from configparser import ConfigParser
+
+def import_rules(rule_filename):
+
+    #imports the rules to a JSON object so we don't keep the file open
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    
+    rf = open(current_dir + "/" + rule_filename)
+    ruledata = json.load(rf)
+    rf.close()
+    
+    return ruledata
+
+def import_cfg(cfg_filename):
+    
+    #imports the config variables
+    global csv_separator 
+    global csv_marker
+    global sourcefile
+    global csv_dataattrib
+    global csv_targetfile
+    
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    configdata = ConfigParser()
+    configdata.read(current_dir + "/" + cfg_filename)
+
+    
+    csv_separator = configdata.get("csv_output","separator") 
+    csv_marker = configdata.get("csv_output","marker")
+    sourcefile = configdata.get("csv_output","sourcefile")
+    csv_dataattrib = configdata.get("csv_output","data_attribute")
+    csv_targetfile = configdata.get("csv_output","targetfile")
+
+    return 0
+
+#Step 1 - import all the rules and config
+rules = import_rules("rules.json")
+config_success = import_cfg("output.cfg")
+
+#Step 2 - open the source file
+sf = open(sourcefile)
+source_data = json.load(sf)
+
+#Step 3 - create the header line
+headingLine = csv_dataattrib 
+for rule in rules['rules']:
+    headingLine += csv_separator + rule["name"] 
+
+#Step 4 - create the target file or clear the file if it exists
+tgtfile = open(csv_targetfile, 'w')
+tgtfile.write(headingLine + "\n")
+
+#Step 5 - generate the report
+for entry in source_data[csv_dataattrib]:
+
+    current_entry = source_data[csv_dataattrib][entry]
+    print (current_entry)
+
+    current_csv_line = entry
+
+    for rule in rules['rules']:
+
+        #the expected attributes for each rule are name:attribute:operator:value
+        attrib = rule["attribute"]
+
+        if rule["operator"] == "equals" :
+            if current_entry[attrib] == rule["value"]:
+                current_csv_line += csv_separator + csv_marker 
+            else:
+                current_csv_line += csv_separator + "" 
+        if rule["operator"] == "return":
+            current_csv_line +=  csv_separator + str(current_entry[attrib])
+        if rule["operator"] == "contains":
+            if rule["value"] in current_entry[attrib]:
+                current_csv_line += csv_separator + csv_marker
+            else:
+                current_csv_line +=  csv_separator +""
+
+    print(current_csv_line)
+    tgtfile.write(current_csv_line + "\n")
+
+tgtfile.close()
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # JSON-Analyser
 Simple Python Script to analyse JSON data with customised rules and create a CSV report
+
+## Introduction
+The purpose of this script is just to take a JSON file with data, apply custom rules (as defined in rules.json) and output a CSV file that can be used for some basic data analysis
+
+
+## Important files
+The 3 main files are as follows:
+
+1. **JSON_Analyser.py** = the main Python script
+2. **rules.json** = a JSON file that contains the custom rules for analysis
+3. **output.cfg** = an ini file that contains the configuration for the output CSV file, such as the source and target filenames, the separator character, etc.
+
+These files all need to be in the same directory on execution.
+
+
+### rules.json
+The expected attributes of a rule are explained below:
+
+1. **name**: Just give a name for your rule. This will also be used for the heading of the respective column in the target CSV
+2. **attribute**: The respective data attribute in the source JSON file that will be inspected
+3. **operator**: What type of operation will be performed on the attribute as part of analysis. The currently supported operators are listed in the next section
+4. **value**: Varies depending on the operator, but in general acts as a control value for the analysis
+
+
+### Supported operator values
+
+- **"equals"**: Checks if the "attribute" is equal to the "value" in the rule. If it is equal, mark in the report.
+- **"return"**: Returns the value of "attribute" from the source JSON as is. The contents of "value" is not checked for this rule.
+- **"contains"**: Checks if the "attribute" contains the string indicated in "value". If it is found, mark in the report.
+
+
+### test.json
+
+A sample file called _test.json_ is included for reference as a basis for the type of JSON files I designed this for.
+
+## TODO
+- Add more operator support (I may add only based on what I need)
